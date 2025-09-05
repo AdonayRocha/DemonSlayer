439943248ea54108e51402455f5cbd717c9a40f9
adonayrocha <adonay.rodrigues.rocha@gmail.com>
2025-09-05 16:31:20 -0300

    Initial Commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import CharacterListScreen from './screens/CharacterListScreen';
+import CharacterDetailScreen from './screens/CharacterDetailScreen';
+
+export type RootStackParamList = {
+  CharacterList: undefined;
+  CharacterDetail: { id: string };
+};
+
+const Stack = createStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="CharacterList"
+          component={CharacterListScreen}
+          options={{ title: 'Demon Slayer' }}
+        />
+        <Stack.Screen
+          name="CharacterDetail"
+          component={CharacterDetailScreen}
+          options={{ title: 'Detalhes' }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
